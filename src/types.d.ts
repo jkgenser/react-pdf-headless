b59df0c9136e07db66b0d2f9cc77c1e4a258e115
jerry genser <jerrygenser@gmail.com>
2024-06-19 11:54:00 -0400

    add: hopefully get things working

---
+++ src/types.d.ts
@@ -0,0 +1,51 @@
+/// <reference types="react" />
+import { VirtualItem } from "@tanstack/react-virtual";
+import { PDFDocumentProxy } from "pdfjs-dist/types/src/display/api";
+import { PageViewport } from "pdfjs-dist//types/src/display/display_utils";
+export interface PageChangeEvent {
+    currentPage: number;
+    doc: PDFDocumentProxy;
+}
+export interface Classes {
+    pagesContainer?: string;
+    pageOuterBox?: string;
+    pageInnerBox?: string;
+    pageWrapper?: string;
+}
+export interface ReaderProps {
+    file: string;
+    initialScale?: number;
+    rotation?: number;
+    onPageChange?: (e: PageChangeEvent) => void;
+    setReaderAPI?: (readerAPI: ReaderAPI) => void;
+    renderPage?: RenderPage;
+    classes?: Classes;
+}
+export interface ReaderAPI {
+    jumpToPage: (pageIndex: number) => void;
+    jumpToHighlightArea: (area: HighlightArea) => void;
+    jumpToOffset: (offset: number) => void;
+}
+export interface HighlightArea {
+    height: number;
+    left: number;
+    top: number;
+    width: number;
+    pageIndex: number;
+}
+export interface RenderPageProps {
+    pageIndex: number;
+    scale: number;
+    rotate: number;
+}
+export type RenderPage = (props: RenderPageProps) => React.ReactNode;
+export interface ReaderPageProps {
+    virtualItem: VirtualItem;
+    viewports: Array<PageViewport>;
+    scale: number | undefined;
+    rotation: number;
+    pageObserver: IntersectionObserver | undefined;
+    shouldRender: boolean;
+    renderPage?: RenderPage;
+    classes?: Classes;
+}
---
+++ src/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: "index.ts",
+    },
+  },
+  plugins: [react()],
+});
