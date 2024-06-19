1c1af409c3bf355c4540dad1351b070d4f373d60
Jerry Genser <jerrygenser@gmail.com>
2024-06-19 12:03:08 -0400

    Fix build (#1)
    * add: hopefully get things working
    
    * adD: yarnrc.ml
    
    * wi p
    
    * add: types
    
    * remove: extra files:
    
    * maybe it works

---
+++ src/index.d.ts
@@ -0,0 +1,3 @@
+import Reader from "./Reader";
+import * as ReaderTypes from "./types";
+export { Reader, ReaderTypes };
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: "./src/index.ts",
+    },
+  },
   plugins: [react()],
-})
+});
