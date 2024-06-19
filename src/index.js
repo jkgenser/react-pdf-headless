68f1036c965d47a88d6ab0d89e9c134ed0b5c231
jerry genser <jerrygenser@gmail.com>
2024-06-19 12:00:21 -0400

    wi p

--- src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ReaderTypes = exports.Reader = void 0;
-var Reader_1 = require("./Reader");
-exports.Reader = Reader_1.default;
-var ReaderTypes = require("./types");
-exports.ReaderTypes = ReaderTypes;
